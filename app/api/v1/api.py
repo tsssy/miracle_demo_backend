bf3762968b2143b3be366aa706873001679f4bdd
Daniel Yu <68982001+dyu056@users.noreply.github.com>
2025-07-21 05:33:18 +0800

    changed name convension

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -4,10 +4,10 @@ from app.api.v1 import UserManagement, MatchManager, ChatroomManager
 api_router = APIRouter()
 
 # 注册用户相关路由
-api_router.include_router(UserManagement.router, prefix="/users", tags=["users"])
+api_router.include_router(UserManagement.router, prefix="/UserManagement", tags=["users"])
 
 # 注册匹配相关路由
 api_router.include_router(MatchManager.router, prefix="/MatchManager", tags=["matches"])
 
 # 注册聊天室相关路由
-api_router.include_router(ChatroomManager.router, prefix="/chatrooms", tags=["chatrooms"]) 
+api_router.include_router(ChatroomManager.router, prefix="/ChatroomManager", tags=["chatrooms"]) 
